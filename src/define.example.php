4b47e2aa5b8acc312161f9c527d5db17d1556c58
Pushkar Anand <anandpushkar088@gmail.com>
2019-10-17 22:58:49 +0530

    added define example.

---
+++ src/define.example.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file contains example config settings for the app.
+ * For the actual app create a file define.php in the src folder with the same values.
+ */
+
+// Database Config
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'DB_USER');
+define('DB_PASS', 'DB_PASS');
+define('DB_NAME', 'DB_NAME');
+
+//Log email config
+define('LOG_REPORT_EMAIL', 'EMAIL_ID');
